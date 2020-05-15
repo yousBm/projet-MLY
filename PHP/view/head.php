0dfa0c8fd34ac3df7a4e018620473000095ff315
Loicdom <loicdomDev@gmail.com>
2020-05-15 09:39:06 +0200

    MAJ php menu head ...

---
+++ PHP/view/head.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8" name="viewport" content="width-device-width,initial-scale=1">
+    <title>Menu</title>
+    <link rel="stylesheet" href="../css/styles.css">
+</head>
