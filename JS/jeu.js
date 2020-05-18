bcaa22cd8ff300efec933da261f2252da53f9a1b
médéric maquinghen <maqmed@msn.com>
2020-05-18 11:24:09 +0200

    HtmlMap1+jsMap1+cssMap1

---
+++ JS/jeu.js
@@ -0,0 +1,103 @@
+//=====================================================================mouvement=====>
+function move(pX, pY) {
+    var move = true;
+    body=document.getElementsByTagName("body")[0];
+    if(body.id=="map1")
+    {
+        var player = document.getElementById("playerMap1"); // joueur
+    }
+    else if (body.id=="map2")
+    {
+        var player = document.getElementById("playerMap2"); // joueur
+    }
+    var stylePlayer = window.getComputedStyle(player, null); // css du joueur
+    var pT = parseInt(stylePlayer.top); // joueur position X
+    var pL = parseInt(stylePlayer.left); // joueur position Y
+    var pW = parseInt(stylePlayer.width); // joueur largeur
+    var pH = parseInt(stylePlayer.height); // joueur hauteur
+
+    var allObstacles = document.querySelectorAll(".obstacle"); // on liste tous les obstacles
+    allObstacles.forEach(function (unObstacle) {
+        var styleObstacle = window.getComputedStyle(unObstacle, null); // css de l'obstacle
+        var oT = parseInt(styleObstacle.top); // obstacle position X
+        var oL = parseInt(styleObstacle.left); // obstacle position Y
+        var oW = parseInt(styleObstacle.width); //obstacle largeur
+        var oH = parseInt(styleObstacle.height); // obstacle hauteur
+        var color = styleObstacle.backgroundColor; // couleur de l'obstacle
+        var id=unObstacle.id;
+
+        move = move && collision(oT, oL, oW, oH, pT + pY, pL + pX, pW, pH, color,id);
+    });
+
+    if (move) {
+            player.style.top = pT + pY + "px";
+            player.style.left = pL + pX + "px";  
+    }
+}
+//=====================================================================collision=====>
+function collision(oT, oL, oW, oH, pT, pL, pW, pH, color,id) {
+    if (pL < oL + oW && pL + pW > oL && pT < oT + oH && pT + pH > oT) {
+        switch (color) {
+            case "rgb(0, 0, 0)":
+                // si on touche un mur
+                break;
+        }
+        switch (id) {
+            case "piece1":
+                piece1=document.getElementById("piece1");
+                piece1.setAttribute("class","invisible");
+                nombrePiece=document.getElementById("nombrePiece").innerHTML;
+                nombrePiece=parseInt(nombrePiece)+1;
+                document.getElementById("nombrePiece").innerHTML=nombrePiece;
+            break;
+            case "piece2":
+                piece2=document.getElementById("piece2");
+                piece2.setAttribute("class","invisible");
+                nombrePiece=document.getElementById("nombrePiece").innerHTML;
+                nombrePiece=parseInt(nombrePiece)+1;
+                document.getElementById("nombrePiece").innerHTML=nombrePiece;
+            break;
+            case "piece3":
+                piece3=document.getElementById("piece3");
+                piece3.setAttribute("class","invisible");
+                nombrePiece=document.getElementById("nombrePiece").innerHTML;
+                nombrePiece=parseInt(nombrePiece)+1;
+                document.getElementById("nombrePiece").innerHTML=nombrePiece;
+            break;
+            case "piece4":
+                piece4=document.getElementById("piece4");
+                piece4.setAttribute("class","invisible");
+                nombrePiece=document.getElementById("nombrePiece").innerHTML;
+                nombrePiece=parseInt(nombrePiece)+1;
+                document.getElementById("nombrePiece").innerHTML=nombrePiece;
+            break;
+            case "porte1":
+                // si on touche l'arrivée
+                // document.location.href="index.php?act=map2";
+            break;    
+        }
+        // playSound("soundCollision", 1, false);
+        return false;
+    }
+    return true;
+}
+//===================================================================deplacement=====>
+document.addEventListener('keydown', function (event) {
+    var speed = 5; // vitesse de déplacement
+    var event = event || window.event, // pour la compatibilite avec tous les navigateurs
+        keyCode = event.keyCode;
+    switch (keyCode) {
+        case 37: // gauche(100)
+            move(-speed, 0);
+            break;
+        case 38: // haut(104)
+            move(0, -speed);
+            break;
+        case 39: // droite(102)
+            move(speed, 0);
+            break;
+        case 40: // bas(98)
+            move(0, speed)
+            break;            
+}
+});
